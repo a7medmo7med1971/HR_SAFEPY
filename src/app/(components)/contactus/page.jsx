80b04c2d752102ed9cef920f65e0204b082102cd
a7medmo7med1971 <ahmed01099229118@gmail.com>
2025-09-07 10:57:35 +0300

    update

--- src/app/(components)/contactus/page.jsx
+++ src/app/(components)/contactus/page.jsx
@@ -64,11 +64,11 @@ export default function ContactPage() {
         <div className="absolute inset-0 bg-black/40" />
 
         {/* المحتوى فوق الصورة */}
-        <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white px-6">
-          <h1 className="text-3xl md:text-5xl font-bold mb-4">
+        <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white px-4">
+          <h1 className="text-3xl md:text-5xl font-bold mb-3">
            تواصل معنا مع HRSafePay
           </h1>
-          <p className="text-lg md:text-xl max-w-xl">
+          <p className="text-lg md:text-lg max-w-lg">
            اتصل بنا أو أرسل رسالة عبر الفورم للحصول على استشارة مجانية وحلول مبتكرة.
           </p>
         </div>
